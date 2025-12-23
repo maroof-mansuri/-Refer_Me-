5d2fbdd14b63b9036864de278a2d7e9749061e92
maroof_mnsr <117035966+maroof-mansuri@users.noreply.github.com>
2025-12-23 23:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import store from "./helpers/store";
+import App from "./containers/App";
+import LoginPage from "./components/pages/Sign in/LoginPage";
+import RegisterPage from "./components/pages/Sign up/RegisterPage";
+import ProfilePage from "./components/pages/Profile/ProfilePage";
+import HomePage from "./components/pages/Home/HomePage";
+import RequestPage from "./components/pages/Requests/RequestPage";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/requests" component={RequestPage} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
